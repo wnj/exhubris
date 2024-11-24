44e1c6817929f8b895d0df8a20cd851dc6af3d1b
Cliff L. Biffle <code@cliffle.com>
2024-11-24 08:30:48 -0800

    userlib: add panic message formatting.

--- sys/userlib/src/lib.rs
+++ sys/userlib/src/lib.rs
@@ -197,6 +197,9 @@ pub enum ReplyFaultReason {
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "no-panic")] {
+
+        // The "panic handler" that won't link, ensuring that no implicit panics
+        // occur.
         #[panic_handler]
         fn panic(_: &core::panic::PanicInfo<'_>) -> !{
             extern "C" {
@@ -210,13 +213,136 @@ cfg_if::cfg_if! {
                 you_have_introduced_a_panic_but_this_task_has_disabled_panics()
             }
         }
+
     } else if #[cfg(feature = "no-panic-messages")] {
+
+        // Panic with a minimal string, rather than formatting the error
+        // message. When combined with LTO this optimizes out all the panic
+        // format machinery.
         #[panic_handler]
         fn panic(_: &core::panic::PanicInfo<'_>) -> ! {
             sys_panic(b"PANIC")
         }
+
     } else {
-        compile_error!("panic support not yet implemented");
+
+        // Relatively expensive panic handler that actually formats the output.
+        #[panic_handler]
+        fn panic(info: &core::panic::PanicInfo<'_>) -> ! {
+            // Theory of operation:
+            //
+            // So, as you might have inferred from the name of this function,
+            // we're panicking right now. Because we're panicking, it is pretty
+            // darn important that we don't panic _more._
+            //
+            // That's impossible to guarantee, because rendering a panic message
+            // can involve calling arbitrary Display impls of arbitrary types,
+            // which could panic. So we'll try our best not to introduce panics
+            // _here_ and hope the caller doesn't write a panicking Display
+            // impl.
+            //
+            // It's also important that we not run out of stack. That is _also_
+            // impossible to prevent in all cases, particularly if the panic
+            // message calls a Debug impl, which tend to be naturally recursive.
+            // (This is what `unwrap` does by default.) But we can at least try
+            // to avoid making things _worse,_ by using statically allocated
+            // memory for most of our state, instead of putting it on the stack.
+            //
+            // Below, we have a very basic implementation of core::fmt::Write
+            // that tries really hard not to panic, and not to put too much on
+            // the stack.
+
+            use core::fmt::Write;
+
+            /// Number of UTF-8 bytes (not characters!) reserved for panic
+            /// messages. Any panic message that exceeds this will be truncated,
+            /// and thus might not be valid UTF-8. Larger values burn more
+            /// static RAM.
+            const PANIC_MESSAGE_MAX: usize = 128;
+            /// The actual reserved buffer.
+            static mut PANIC_BUFFER: [u8; PANIC_MESSAGE_MAX] = [0; PANIC_MESSAGE_MAX];
+
+            /// A writer that spits bytes into a limited-length buffer.
+            struct LimitedWriter {
+                /// The output.
+                buf: &'static mut [u8; PANIC_MESSAGE_MAX],
+                /// The position, which may be past the end of the buffer, in
+                /// which case we don't produce more output.
+                pos: usize,
+            }
+
+            impl Write for LimitedWriter {
+                fn write_str(&mut self, s: &str) -> core::fmt::Result {
+                    // If the buffer is already full, this is a no-op.
+                    //
+                    // We're using `>=` here even though we don't expect pos to
+                    // _pass_ buf.len(), because we're about to do some unsafe
+                    // and it'd be a real shame to access off the end of the
+                    // buffer.
+                    if self.pos >= self.buf.len() {
+                        return Ok(());
+                    }
+
+                    // Slice the remaining section of the buffer.
+                    //
+                    // Safety: we have checked the range above.
+                    let remaining = unsafe { self.buf.get_unchecked_mut(self.pos..) };
+
+                    // Compute the size of the prefix of `s` that we can write
+                    // into our remaining slice.
+                    let strbytes = s.as_bytes();
+                    let to_write = usize::min(remaining.len(), strbytes.len());
+
+                    // Copy the bytes. We are not using `slice::copy_from_slice`
+                    // because it contains panics, and while the compiler can
+                    // _usually_ eliminate them, that's a tenuous guarantee.
+                    //
+                    // Safety: Both pointers are valid since we're deriving them
+                    // from slices we hold. `to_write` is the minimum of the two
+                    // lengths. So, this should be sound.
+                    unsafe {
+                        core::ptr::copy_nonoverlapping(
+                            strbytes.as_ptr(),
+                            remaining.as_mut_ptr(),
+                            to_write,
+                        );
+                    }
+
+                    // Advance our position. We use a wrapping add here to
+                    // suppress overflow checks (and thus panics). A wrapping
+                    // add is equivalent to a non-wrapping add in this case
+                    // because, due to how we computed `to_write` above, `pos +
+                    // to_write` is less than or equal to `pos.len()`, which
+                    // fits in a `usize` without wrapping.
+                    self.pos = self.pos.wrapping_add(to_write);
+
+                    Ok(())
+                }
+            }
+
+            // Grab a mutable reference to our static buffer.
+            //
+            // Safety: alright. so. this can technically produce an aliasing
+            // &mut in the case where we are panicking recursively. However, the
+            // &mut taken in the earlier panic handler invocation is lost,
+            // because the routine panicked and we'll never return to it. It's
+            // not clear that a formalism like stacked borrows recognizes this,
+            // but I'm pretty sure it's the best we can do.
+            let buf = unsafe { &mut *core::ptr::addr_of_mut!(PANIC_BUFFER) };
+
+            // Render the panic message!
+            let mut writer = LimitedWriter { buf, pos: 0 };
+            write!(writer, "{}", info).ok();
+
+            // Pass the rendered portion of our buffer to the kernel.
+            //
+            // Safety: LimitedWriter takes pains to ensure that `pos` does not
+            // pass `buf.len()`, so `get_unchecked` here is fine. We are using
+            // `get_unchecked` to avoid the panic in `Index`, which the compiler
+            // can often prove can't happen, but that's not guaranteed.
+            sys_panic(unsafe { writer.buf.get_unchecked(..writer.pos) })
+        }
+
     }
 }
 
