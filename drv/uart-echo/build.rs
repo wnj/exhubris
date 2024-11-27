2841b5320530713f85d6baf73658d3f82ccdb407
Cliff L. Biffle <code@cliffle.com>
2024-11-26 16:38:29 -0800

    uart-echo: make config parsing...less typed?
    Weirdly, handling the config as actual JSON has some compelling DRY
    properties, and avoids the need to pull serde+deriving into the build
    script. So it's overall less for the application to manage.
    
    As long as the schema can be validated, it's not even all that risky.

--- drv/uart-echo/build.rs
+++ drv/uart-echo/build.rs
@@ -1,14 +1,14 @@
 use std::path::PathBuf;
 use std::io::Write;
 
-use serde::Deserialize;
+use serde_json::{Map, Value};
 
 fn main() {
     println!("cargo::rerun-if-env-changed=HUBRIS_TASK_CONFIG");
     let Ok(configstr) = std::env::var("HUBRIS_TASK_CONFIG") else {
         panic!("Missing config for task!");
     };
-    let config: Config = serde_json::from_str(&configstr).unwrap();
+    let config: Map<String, Value> = serde_json::from_str(&configstr).unwrap();
 
     let mut out = PathBuf::from(std::env::var("OUT_DIR").unwrap());
     out.push("task_config.rs");
@@ -18,13 +18,13 @@ fn main() {
     writeln!(f, "pub(crate) mod config {{").unwrap();
     writeln!(f, "use drv_stm32g0_sys_api::Port;").unwrap();
 
-    writeln!(f, "pub const UART_CLOCK_HZ: u32 = {};", config.uart_clock_hz).unwrap();
-    writeln!(f, "pub const BAUD_RATE: u32 = {};", config.baud_rate).unwrap();
+    writeln!(f, "pub const UART_CLOCK_HZ: u32 = {};", config["uart-clock-hz"]).unwrap();
+    writeln!(f, "pub const BAUD_RATE: u32 = {};", config["baud-rate"]).unwrap();
 
     writeln!(f, "pub const PINS: [(Port, u8, u8); 2] = [").unwrap();
-    for pinaf in &config.pins {
-        let (port, pin) = parse_pin_name(&pinaf.name);
-        writeln!(f, "    (Port::{port}, {pin}, {}),", pinaf.af).unwrap();
+    for pinaf in config["pins"].as_array().unwrap() {
+        let (port, pin) = parse_pin_name(pinaf["name"].as_str().unwrap());
+        writeln!(f, "    (Port::{port}, {pin}, {}),", pinaf["af"].as_u64().unwrap()).unwrap();
     }
     writeln!(f, "];").unwrap();
     writeln!(f, "}}").unwrap();
@@ -34,18 +34,4 @@ fn parse_pin_name(name: &str) -> (&str, u8) {
     let (port, pin) = name.split_at(1);
 
     (port, pin.parse().unwrap())
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "kebab-case")]
-struct Config {
-    uart_clock_hz: u32,
-    baud_rate: u32,
-    pins: [PinAf; 2],
-}
-
-#[derive(Debug, Deserialize)]
-struct PinAf {
-    name: String,
-    af: u8,
 }
