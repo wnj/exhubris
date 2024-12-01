ff8c3153a9486f02d113a1a267f48cabed3e17b6
Cliff L. Biffle <code@cliffle.com>
2024-11-30 17:20:21 -0800

    drv/stm32l4-sys-api

---
+++ drv/stm32l4-sys-api/build.rs
@@ -0,0 +1,13 @@
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo::rerun-if-changed=../stm32l4-sys/sys-idl.kdl");
+    let iface = tools::idl::load_interface("../stm32l4-sys/sys-idl.kdl").unwrap();
+    let client = tools::idl::codegen::generate_client(&iface).unwrap();
+    let client = tools::idl::codegen::format_code(&client);
+    let mut outpath = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    outpath.push("generated_client.rs");
+
+    std::fs::write(&outpath, client).unwrap();
+
+}
