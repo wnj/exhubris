a9ed7fdfc691e7900357e5821605f71f68bca583
Cliff L. Biffle <code@cliffle.com>
2024-12-09 21:35:25 -0800

    idle: add insomniac feature for Humility

--- task/idle/src/main.rs
+++ task/idle/src/main.rs
@@ -6,6 +6,16 @@ use userlib as _;
 #[export_name = "main"]
 fn main() -> ! {
     loop {
-        userlib::idle();
+        if cfg!(feature = "insomniac") {
+            // It may be useful, on some systems, to stop the CPU from entering
+            // low power mode. Usually this is done because low power mode
+            // interrupts debugging.
+            //
+            // This is not an empty block because that would reduce to `loop
+            // {}`, which the compiler tends to replace with a trap instruction.
+            core::sync::atomic::compiler_fence(core::sync::atomic::Ordering::SeqCst);
+        } else {
+            userlib::idle();
+        }
     }
 }
