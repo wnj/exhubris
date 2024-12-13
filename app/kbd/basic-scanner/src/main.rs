b105e4466a91b7dd3baaeafcb0c2d6f7ee81b463
Cliff L. Biffle <code@cliffle.com>
2024-12-12 19:37:18 -0800

    kbd: switch scanner to use TIM2, scan faster

--- app/kbd/basic-scanner/src/main.rs
+++ app/kbd/basic-scanner/src/main.rs
@@ -17,17 +17,20 @@
 use core::mem::MaybeUninit;
 
 use hubris_task_slots::SLOTS;
-use drv_stm32l4_sys_api::{Stm32L4Sys as Sys, Port, Pull};
+use drv_stm32l4_sys_api::{Stm32L4Sys as Sys, Port, Pull, PeripheralName};
 use idyll_runtime::NotificationHandler;
-use userlib::{Message, ReplyFaultReason, TaskId};
+use userlib::{sys_enable_irq_and_clear_pending, Message, ReplyFaultReason, TaskId};
 
-const INTERVAL: u64 = 1; // millisecond
+const INTERVAL: u32 = 62 - 15; // microseconds; fudge factor adjusts for code
+                               // execution time
 
 const QUEUE_DEPTH: usize = 16;
 
 #[export_name = "main"]
 fn main() -> ! {
     let sys = Sys::from(SLOTS.sys);
+
+    sys.enable_clock(PeripheralName::Tim2);
 
     for (port, pin) in config::ROWS {
         sys.set_pin_output(port, pin);
@@ -38,14 +41,43 @@ fn main() -> ! {
         sys.set_pin_input(port, pin);
     }
 
-    // Record the current time so we can manage our timer properly.
-    let next_time = userlib::sys_get_timer().now + INTERVAL;
-    userlib::sys_set_timer(Some(next_time), hubris_notifications::TIMER);
+    // Alright, we're done using SYS for performance reasons, start poking the
+    // GPIO directly.
+    for (port, pin) in config::ROWS {
+        set_pin_low(port, pin);
+    }
+
+    let tim = stm32_metapac::TIM2;
+    // By default the timer is receiving 80 MHz from the bus fabric. Divide that
+    // by 80 to give a microsecond timebase.
+    tim.psc().write(|w| w.set_psc(80 - 1));
+    // Let's wake up every INTERVAL us.
+    tim.arr().write(|w| w.set_arr(INTERVAL - 1));
+    // Trigger an update event so those registers get applied.
+    tim.egr().write(|w| w.set_ug(true));
+    // Clear any status bits that just got set by that. All writable bits in
+    // this register are write-zero-to-clear.
+    tim.sr().write(|w| w.0 = 0);
+    // Enable interrupt on timer overflow (update).
+    tim.dier().write(|w| w.set_uie(true));
+    // Start the timer!
+    tim.cr1().write(|w| {
+        // One-pulse mode causes the timer to interrupt once and turn itself
+        // off, which gets us our "at least" sleep behavior more effectively
+        // than a cyclic repeating interrupt would ... but it does mean we have
+        // to remember to turn it back on!
+        w.set_opm(true);
+        w.set_cen(true);
+    });
+
+    // Turn on our IRQ. It'll fire real soon now but we won't notice until we
+    // recv.
+    sys_enable_irq_and_clear_pending(hubris_notifications::TIM_IRQ);
 
     // Event loop!
     let mut buffer = [MaybeUninit::uninit(); SCANNER_BUFFER_SIZE];
     let mut server = Server {
-        next_time,
+        tim,
         scan_row: 0,
         keys_down: [[false; config::COLS.len()]; config::ROWS.len()],
         queue: heapless::Deque::new(),
@@ -54,14 +86,14 @@ fn main() -> ! {
     loop {
         idyll_runtime::dispatch_or_event(
             &mut server,
-            hubris_notifications::TIMER,
+            hubris_notifications::TIM_IRQ,
             &mut buffer,
         );
     }
 }
 
 struct Server {
-    next_time: u64,
+    tim: stm32_metapac::timer::TimGp32,
     scan_row: usize,
     keys_down: [[bool; config::COLS.len()]; config::ROWS.len()],
 
@@ -81,14 +113,22 @@ impl Scanner for Server {
 
 impl NotificationHandler for Server {
     fn handle_notification(&mut self, bits: u32) {
-        if bits & hubris_notifications::TIMER != 0 {
+        if bits & hubris_notifications::TIM_IRQ != 0 {
             // Advance key scanning, but only if time has really elapsed. (This
             // check is me being pedantic: it's possible for other tasks to post
             // our timer notification instead of the kernel doing it, so, I
             // check the time.)
-            let now = userlib::sys_get_timer().now;
-            if now >= self.next_time {
-                // A real timer event!
+            if self.tim.sr().read().uif() {
+                // A real timer event! Clear the interrupt condition at the
+                // timer...
+                self.tim.sr().write(|w| {
+                    // write 1 to preserve
+                    w.0 = !0;
+                    // ...except the update flag
+                    w.set_uif(false);
+                });
+                // ...and re-enable in the kernel.
+                sys_enable_irq_and_clear_pending(hubris_notifications::TIM_IRQ);
 
                 // Read column inputs
                 let downs = &mut self.keys_down[self.scan_row];
@@ -127,9 +167,9 @@ impl NotificationHandler for Server {
                 self.scan_row = (self.scan_row + 1) % config::ROWS.len();
                 set_pin_high(config::ROWS[self.scan_row].0, config::ROWS[self.scan_row].1);
 
-                // Bump our timer to the next deadline.
-                self.next_time += INTERVAL;
-                userlib::sys_set_timer(Some(self.next_time), hubris_notifications::TIMER);
+                // Restart our timer. We do this _after_ turning on the GPIO
+                // above so that it acts as a propagation delay before we read.
+                self.tim.cr1().modify(|w| w.set_cen(true));
             }
         }
     }
