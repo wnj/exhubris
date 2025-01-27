7b0983e0c022c68dbd24bef6f6e9c767955a22d0
Cliff L. Biffle <code@cliffle.com>
2025-01-27 08:42:36 -0800

    build: clean up and document the relink functions

--- tools/build/src/main.rs
+++ tools/build/src/main.rs
@@ -6,7 +6,7 @@ use goblin::elf::program_header::PT_LOAD;
 use miette::{bail, miette, Context, IntoDiagnostic as _, LabeledSpan, NamedSource};
 use rangemap::RangeMap;
 use size::Size;
-use hubris_build::{alloc::{allocate_space, TaskAllocation}, appcfg::{self, AppDef, BuildMethod, BuildPlan}, buildid::BuildId, get_target_spec, BuildEnv, TargetSpec};
+use hubris_build::{alloc::{allocate_space, TaskAllocation}, appcfg::{self, AppDef, BuildMethod, BuildPlan, TaskDef}, buildid::BuildId, get_target_spec, BuildEnv, TargetSpec};
 use hubris_region_alloc::{Mem, TaskInfo, TaskName};
 use hubris_build_kconfig as kconfig;
 
@@ -148,7 +148,7 @@ fn main() -> miette::Result<()> {
                     &app,
                     &target_spec,
                     &targetroot,
-                    taskname,
+                    &app.tasks[taskname],
                     &initial_build_dir.join(taskname),
                     &temp_link_dir.join(taskname),
                     &workdir.join("task-link2.x"),
@@ -260,7 +260,7 @@ fn main() -> miette::Result<()> {
                     &app,
                     &target_spec,
                     &targetroot,
-                    taskname,
+                    &app.tasks[taskname],
                     &initial_build_dir.join(taskname),
                     &dir3.join(taskname),
                     &workdir.join("task-link3.x"),
@@ -823,27 +823,63 @@ fn simple_table<'a>(content: impl IntoIterator<Item = (&'a str, &'a str)>) {
     println!();
 }
 
+/// Performs a second link pass on a partially linked executable.
+///
+/// `env`, `app`, `target`, and `targetroot` describe the overall build
+/// configuration.
+///
+/// `def` specifies the task in the `app`. This should be a reference to one of
+/// the `app.tasks`.
+///
+/// `inpath` gives the location of the partially linked executable, while
+/// `outpath` will be written with the result.
+///
+/// `linker_script` gives the location of the linker script to use in this pass.
+/// The script should contain a directive to include `memory.x`.
+///
+/// `alloc` is the results of memory allocation for the task, after the initial
+/// build and analysis phase. It is a map from memory name (as given in the
+/// chipdef) to allocation requirements.
+///
+/// On success, returns:
+/// - A map from memory name to regions owned by this task.
+/// - The task's initial stack pointer, in terms of those regions.
 #[allow(clippy::too_many_arguments)]
 fn relink(
     env: &BuildEnv,
     app: &AppDef,
     target: &TargetSpec,
     targetroot: &Path,
-    taskname: &str,
+    def: &TaskDef,
     inpath: &Path,
     outpath: &Path,
     linker_script: &Path,
     alloc: &BTreeMap<String, TaskAllocation>,
 ) -> miette::Result<(BTreeMap<String, OwnedRegion>, Option<OwnedAddress>)> {
+    // Generate the linker script "memory fragment" in the targetroot. This file
+    // has to be called "memory.x" because it's pulled in by an include
+    // directive under that name by the reusable part of the linker script.
+    //
+    // This fragment consists of
+    // - The MEMORY section
+    // - A SECTIONS add-on stanza containing any additional section mappings
+    //   required for the task.
     let memory_frag_path = targetroot.join("memory.x");
     let mut scr = std::fs::File::create(&memory_frag_path)
         .into_diagnostic()?;
     writeln!(scr, "MEMORY {{").into_diagnostic()?;
-    let def = &app.tasks[taskname];
+
+    // During this same pass over the task memory map, we're going to determine
+    // the value of the initial stack pointer and the set of owned memory
+    // regions.
     let mut initial_stack_pointer = None;
     let mut owned_regions = BTreeMap::new();
+
+    // For each memory type defined in the chipdef, check if the task has
+    // any requirements in that memory.
     for orig_name in app.board.chip.memory.keys() {
         let Some(regalloc) = alloc.get(orig_name) else {
+            // Nope!
             continue;
         };
 
@@ -852,16 +888,24 @@ fn relink(
             sizes: regalloc.sizes.clone(),
         });
 
+        // Linker memory names are conventionally UPPERCASE, so:
         let name = orig_name.to_ascii_uppercase();
+
         let mut base = regalloc.base;
         let mut size = regalloc.calculate_size();
 
         if name == "RAM" {
-            // deduct stack
+            // We recognize this magical memory name as the place where stacks
+            // go. Stacks are inserted at the bottom of the task's RAM area.
+            //
+            // TODO: the fact that this uses the memory name is kind of grody.
 
+            // We generate a "memory" named STACK to describe this.
             writeln!(scr, "STACK (rw): ORIGIN = {base:#x}, LENGTH = {:#x}", def.stack_size.value()).into_diagnostic()?;
+            // Deduct the stack from the overall RAM size.
             base += *def.stack_size.value();
             size -= *def.stack_size.value();
+            // The stack pointer will start at the top of that deduction.
             initial_stack_pointer = Some(OwnedAddress {
                 region: orig_name.to_string(),
                 offset: *def.stack_size.value(),
@@ -870,18 +914,31 @@ fn relink(
         writeln!(scr, "{name} (rw): ORIGIN = {base:#x}, LENGTH = {size:#x}").into_diagnostic()?;
     }
     writeln!(scr, "}}").into_diagnostic()?;
+
+    // And now, the SECTIONS section, to insert sections with your sections so
+    // you can section your sections. ...
+    //
+    // Anyway, this handles any explicitly declared additional memory sections.
     writeln!(scr, "SECTIONS {{").into_diagnostic()?;
-    for (section, memory) in &app.tasks[taskname].sections {
+    for (section, memory) in &def.sections {
+        // We tolerate section names not starting with a dot in the config, but
+        // for consistency, add a dot in the linker script.
         let leader = if section.starts_with('.') { "" } else { "." };
+
         let memory = memory.to_ascii_uppercase();
         writeln!(scr, "{leader}{section} (NOLOAD) : ALIGN(4) {{").into_diagnostic()?;
         writeln!(scr, "  *({leader}{section} {leader}{section}.*);").into_diagnostic()?;
         writeln!(scr, "}} > {memory}").into_diagnostic()?;
     }
     writeln!(scr, "  ").into_diagnostic()?;
+    // .uninit is our final useful section in the linker script, and generally
+    // where things like this want to go.
     writeln!(scr, "}} INSERT AFTER .uninit").into_diagnostic()?;
+
+    // All done!
     drop(scr);
 
+    // Run the linker on the provided linker script, plus our fragment.
     let mut ldcmd = cmd_with_clean_env(&env.linker_path);
     ldcmd.arg(inpath);
     ldcmd.arg("-o").arg(outpath);
@@ -900,21 +957,40 @@ fn relink(
         return Err(miette!("command failed"));
     }
 
+    // Delete the memory fragment file, to avoid surprises.
     std::fs::remove_file(memory_frag_path).into_diagnostic()?;
+
     Ok((owned_regions, initial_stack_pointer))
 }
 
+/// Re-links a partially linked executable against a bogus memory map, to
+/// determine its resource requirements.
+///
+/// Specifically, this links a partially-linked task as though it had sole
+/// control of all of the memory in the chipdef. We then analyze the resulting
+/// ELF executable to determine memory requirements in each memory region, and
+/// return that.
+///
+/// `env`, `app`, `target`, and `targetroot` define the build environment.
+///
+/// `def` defines the task, and should be a reference to one of `app.tasks`.
+///
+/// `inpath` gives the path to the partially-linked ELF executable, and
+/// `outpath` gives a location where we can write a linked-but-bogus equivalent.
+///
+/// `linker_script` is a path to the linker script to use at this stage.
 #[allow(clippy::too_many_arguments)]
 fn relink_for_size(
     env: &BuildEnv,
     app: &AppDef,
     target: &TargetSpec,
     targetroot: &Path,
-    taskname: &str,
+    def: &TaskDef,
     inpath: &Path,
     outpath: &Path,
     linker_script: &Path,
 ) -> miette::Result<BTreeMap<String, Range<u64>>> {
+    // Make an "allocation result" that assigns this task all of memory.
     let alloc_everything = app.board.chip.memory.iter()
         .map(|(name, regdef)| (name.clone(), TaskAllocation {
             requested: 0,
@@ -922,27 +998,42 @@ fn relink_for_size(
             sizes: vec![*regdef.value().size.value()],
         }))
     .collect();
-    relink(env, app, target, targetroot, taskname, inpath, outpath, linker_script, &alloc_everything)?;
+    // Relink the task to that allocation.
+    relink(env, app, target, targetroot, def, inpath, outpath, linker_script, &alloc_everything)?;
 
+    // Load the result and analyze the memory layout.
     let file_image = std::fs::read(outpath).into_diagnostic()?;
     let elf = goblin::elf::Elf::parse(&file_image).into_diagnostic()?;
 
+    // Initialize the size map with an entry for each type of memory defined in
+    // the chipdef, but with a zero-size range at its base. We will enlarge
+    // these ranges as required.
     let mut region_sizes = app.board.chip.memory.iter()
         .map(|(name, region)| (name.clone(), *region.value().base.value()..*region.value().base.value()))
         .collect::<BTreeMap<_, _>>();
+
     for phdr in &elf.program_headers {
+        // Skip metadata and whatnot.
         if phdr.p_type != PT_LOAD {
             continue;
         }
+        // Figure out which region contains this phdr.
         let (regname, _reg) = app.board.chip.memory.iter()
             .find(|(_name, reg)| *reg.value().base.value() <= phdr.p_vaddr && (phdr.p_vaddr + phdr.p_memsz) <= (reg.value().base.value() + reg.value().size.value()))
             .expect("internal inconsistency");
 
+        // Borrow its size range in our output table.
         let sz = region_sizes.get_mut(regname).expect("internal inconsistency");
+        // Enlarge the range to contain all of this item.
         sz.start = u64::min(sz.start, phdr.p_vaddr);
         sz.end = u64::max(sz.end, phdr.p_vaddr + phdr.p_memsz);
 
         if phdr.p_vaddr != phdr.p_paddr {
+            // This is an "initialized data" phdr, like the .data section. This
+            // means it appears twice, once in RAM at its full memsz, and once
+            // somewhere else (generally flash) with a possibly smaller filsz.
+            //
+            // Repeat the process, but use the paddr and filsz.
             let (regname, _reg) = app.board.chip.memory.iter()
                 .find(|(_name, reg)| *reg.value().base.value() <= phdr.p_paddr && (phdr.p_paddr + phdr.p_filesz) <= (reg.value().base.value() + reg.value().size.value()))
                 .expect("internal inconsistency");
@@ -955,20 +1046,43 @@ fn relink_for_size(
     Ok(region_sizes)
 }
 
+/// Relink a partially-linked task executable in its final location.
+///
+/// `env`, `app`, `target`, and `targetroot` define the build environment.
+///
+/// `def` defines the task, and should be a reference to one of `app.tasks`.
+///
+/// `inpath` gives the path to the partially-linked ELF executable, and
+/// `outpath` is where the final linked result will be placed.
+///
+/// `linker_script` is the path to the linker script we'll use for the final
+/// link.
+///
+/// `alloc` is the set of allocation decisions for this task, defining its final
+/// placement.
+/// 
+/// On success, returns a `BuiltTask` summarizing the output.
 #[allow(clippy::too_many_arguments)]
 fn relink_final(
     env: &BuildEnv,
     app: &AppDef,
     target: &TargetSpec,
     targetroot: &Path,
-    taskname: &str,
+    def: &TaskDef,
     inpath: &Path,
     outpath: &Path,
     linker_script: &Path,
     alloc: &BTreeMap<String, TaskAllocation>,
 ) -> miette::Result<BuiltTask> {
-    let (owned_regions, initial_stack_pointer) = relink(env, app, target, targetroot, taskname, inpath, outpath, linker_script, alloc)?;
+    // Use the common relink routine to do most of the work.
+    let (owned_regions, initial_stack_pointer) = relink(env, app, target, targetroot, def, inpath, outpath, linker_script, alloc)?;
 
+    // There's one specific piece of information that we don't have yet: the
+    // binary's entry address. While most Hubris binaries build themselves to
+    // have their entry point at the base of flash, we don't actually _require_
+    // this.
+    //
+    // Instead, we're going to parse the ELF headers.
     let file_image = std::fs::read(outpath).into_diagnostic()?;
     let elf = goblin::elf::Elf::parse(&file_image).into_diagnostic()?;
 
@@ -983,10 +1097,9 @@ fn relink_final(
     let initial_stack_pointer = initial_stack_pointer.expect("missing RAM region?");
 
     Ok(BuiltTask {
-        name: taskname.to_string(),
+        name: def.name.clone(),
         entry,
         initial_stack_pointer,
         owned_regions,
     })
 }
-
